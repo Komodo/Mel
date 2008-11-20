da6cbd692fdd01fe01f5c788962d83044ef8f7fa
Todd Whiteman <toddw@activestate.com>
2008-11-20 16:26:38 +0000

    maya: update lang_from_udl_family and comment styles
    git-svn-id: http://svn.openkomodo.com/repos/addons/maya@2605 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- components/koMel_UDL_Language.py
+++ components/koMel_UDL_Language.py
@@ -21,27 +21,10 @@ class KoMelLanguage(KoUDLLanguage):
     _reg_clsid_ = "78c36838-be2a-4193-b154-81d7026458c4"
     defaultExtension = '.mel'
 
-    #TODO: Update 'lang_from_udl_family' as appropriate for your
-    #      lexer definition. There are four UDL language families:
-    #           M (markup), i.e. HTML or XML
-    #           CSL (client-side language), e.g. JavaScript
-    #           SSL (server-side language), e.g. Perl, PHP, Python
-    #           TPL (template language), e.g. RHTML, Django, Smarty
-    #      'lang_from_udl_family' maps each UDL family code (M,
-    #      CSL, ...) to the sub-language name in your language.
-    #      Some examples:
-    #        lang_from_udl_family = {   # A PHP file can contain
-    #           'M': 'HTML',            #   HTML
-    #           'SSL': 'PHP',           #   PHP
-    #           'CSL': 'JavaScript',    #   JavaScript
-    #        }
-    #        lang_from_udl_family = {   # An RHTML file can contain
-    #           'M': 'HTML',            #   HTML
-    #           'SSL': 'Ruby',          #   Ruby
-    #           'CSL': 'JavaScript',    #   JavaScript
-    #           'TPL': 'RHTML',         #   RHTML template code
-    #        }
-    #        lang_from_udl_family = {   # A plain XML can just contain
-    #           'M': 'XML',             #   XML
-    #        }
-    lang_from_udl_family = {}
+    lang_from_udl_family = {
+        'SSL': 'Mel'
+    }
+
+    commentDelimiterInfo = {
+        "line": [ "//", ],
+    }
