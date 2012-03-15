de2544f0d5d5e2ad4b3c79bee001501f05333458
Todd Whiteman <toddw@activestate.com>
2012-03-15 18:37:54 +0000

    addons: maya: update for Komodo 7
    git-svn-id: http://svn.openkomodo.com/repos/addons/maya@9920 b87d16e7-d29f-4ee1-b5ec-817d645d335f

--- components/koMel_UDL_Language.py
+++ components/koMel_UDL_Language.py
@@ -18,6 +18,7 @@ class KoMelLanguage(KoUDLLanguage):
     lexresLangName = "Mel"
     _reg_desc_ = "%s Language" % name
     _reg_contractid_ = "@activestate.com/koLanguage?language=%s;1" % name
+    _reg_categories_ = [("komodo-language", name)]
     _reg_clsid_ = "78c36838-be2a-4193-b154-81d7026458c4"
     defaultExtension = '.mel'
 
